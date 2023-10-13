031d1ace4f19bb718cfaddf2765f176c43a606c9
Anjali <109861978+Anj13Niki@users.noreply.github.com>
2023-10-13 16:14:56 +0000

    datatype

--- .devcontainer/01basics/02datatype.js
+++ .devcontainer/01basics/02datatype.js
@@ -4,3 +4,18 @@
 let fanme ="Anjali";
 let age = 21;
 let isLogin = true;
+
+let s = "21";
+let num = Number(s);
+let sc = "21adi";
+// String Type
+console.log(typeof(s));
+// Integer type
+console.log(typeof(num));
+
+console.log(typeof(sc));
+
+// type of sc number
+console.log(typeof(Number(sc)));
+// value of sc is NAN
+console.log(Number(sc))
